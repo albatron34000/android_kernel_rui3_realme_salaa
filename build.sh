fc7569b1849876fc961fef2da986fdd464ef0670
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-09-08 11:31:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,20 +29,15 @@ make -j$(nproc --all) O=out \
                       LD=ld.lld \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
-function zupload()
-{
-zimage=out/arch/arm64/boot/Image.gz
-if ! [ -a $zimage ];
-then
-echo  " Failed To Compile Kernel"
-else
-echo -e " Kernel Compile Successful"
-git clone --depth=1 git clone --depth=1 https://github.com/albatron34000/AnyKernel3_a15.git AnyKernel
-cp out/arch/arm64/boot/Image.gz AnyKernel
-cd AnyKernel
-zip -r9 Stormbreaker-salaa-${TANGGAL}.zip *
-cd ../
-fi
+setup_kernel_release() {
+    # setup_kernel_release
+    v=$(cat version)
+    d=$(date "+%d%m%Y")
+    z="stormbreaker-kernel-salaa-$d-$v-ksu.zip"
+    wget --quiet https://psionicprjkt.my.id/assets/files/AK3-salaa.zip && unzip AK3-salaa
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel && cd AnyKernel
+    zip -r9 "$z" *
 }
+
 compile
-zupload
+setup_kernel_release
